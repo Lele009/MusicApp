871ee4f8697ab5cb596de26acf704e2ef67a114c
Luca Nocella <lucaboom1993@hotmail.com>
2018-01-29 15:25:03 +0100

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Luca Nocella on 29/01/18.
+//  Copyright © 2018 Luca Nocella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
